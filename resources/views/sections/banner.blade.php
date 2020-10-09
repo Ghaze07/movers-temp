e6add600b8df0c6581a6a3d7f02f9018f94dc312
Ghaze07 <ghaziislam3@gmail.com>
2020-10-09 10:53:05 +0500

    Front page modified

--- resources/views/sections/banner.blade.php
+++ resources/views/sections/banner.blade.php
@@ -6,6 +6,9 @@
                 <div class="col-12 justify-content-around text-right pb-1 pr-4">
                     <h1>ZAYNAB FISH FARM</h1>
                     <h6>IMPORTED SEED - FARM FRESH - ORGANIC - HEALTHY</h6>
+                    <a href="https://wa.me/923429296252?text=Hi%2C+I+am+interested+in+buying+fish+and+would+like+to+place+my+order+through+WhatsApp." target="_blank">
+                        <img src="{{ asset('pix/icon_whatsapp.png') }}">
+                    </span></a>
                     <a href="tel:+92 342 5538 615" class="btn btn-lg" style="background: #F9C909;"><i class="fas fa-headset"></i><span class="pl-2">Call Us</span></a>
                     <a href="#ordernow" class="btn btn-lg" style="background: #FD565A;"><i class="fas fa-shopping-basket"></i><span class="pl-2">Order Online</span></a>
                 </div>
