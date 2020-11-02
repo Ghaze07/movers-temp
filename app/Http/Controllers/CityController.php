81853230c3197944fb538fdea961a2cb51cb2abc
khyam-shahzad <khyam.shahzad@gmail.com>
2020-11-02 11:54:47 +0300

    Basic CRUD functionality for Country, Region and City controllers.

--- app/Http/Controllers/CityController.php
+++ app/Http/Controllers/CityController.php
@@ -3,16 +3,127 @@
 namespace App\Http\Controllers;
 
 use App\City;
+use App\Region;
+use App\Country;
 use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Session;
 
 class CityController extends Controller
 {
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
     public function index()
     {
         $cities = City::with('region')->orderBy('region_id', 'asc')
-            ->orderBy('name', 'asc')->paginate(20);
+                        ->orderBy('name', 'asc')->paginate(20);
+        $regions = Region::where(['status' => 1])->orderBy('name', 'asc')->get();
+        $countries = Country::where(['status' => 1])->orderBy('name', 'asc')->get();
+
         return view('city.index')->with([
-            'cities' => $cities
+            'cities' => $cities,
+            'regions' => $regions,
+            'countries' => $countries
         ]);
     }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $data = $this->validate($request, [
+            'country' => 'required',
+            'region' => 'required',
+            'name' => 'required',
+            'status' => 'required',
+        ]);
+
+        $city = new City;
+        $city->name = $data['name'];
+        $city->status = $data['status'];
+        $city->region_id = $data['region'];
+        $city->save();
+
+        Session::flash('statuscode', 'success');
+        return redirect('/cities')->with('status', 'City has been added successfully.');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, City $city)
+    {
+        $data = $this->validate($request, [
+            'name' => 'required',
+            'region' => 'required',
+            'country' => 'required',
+            'status' => 'required',
+        ]);
+
+        $city->name = $data['name'];
+        $city->region_id = $data['region'];
+        $city->status = $data['status'];
+        $city->update();
+
+        Session::flash('statuscode', 'success');
+        return redirect('/cities')->with('status', 'City has been updated.');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        $city = City::findorfail($id);
+        $city->delete();
+
+        return redirect('/cities')->with('status', 'City has been deleted.');
+    }
+
 }
--- app/Http/Controllers/RegionController.php
+++ app/Http/Controllers/RegionController.php
@@ -3,16 +3,120 @@
 namespace App\Http\Controllers;
 
 use App\Region;
+use App\Country;
 use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Session;
 
 class RegionController extends Controller
 {
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
     public function index()
     {
         $regions = Region::with('country')->orderBy('country_id', 'asc')
                                                     ->orderBy('name', 'asc')->paginate(20);
+        $countries = Country::where(['status' => 1])->orderBy('name', 'asc')->get();
         return view('region.index')->with([
-            'regions' => $regions
+            'regions' => $regions,
+            'countries' => $countries
         ]);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $data = $this->validate($request,[
+            'name' => 'required',
+            'country' => 'required',
+            'status' => 'required',
+        ]);
+
+        $region = new Region;
+        $region->name = $data['name'];
+        $region->country_id = $data['country'];
+        $region->status = $data['status'];
+        $region->save();
+
+        Session::flash('statuscode', 'success');
+        return redirect('/regions')->with('status', 'Region added successfully.');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request,  Region $region)
+    {
+        $data = $this->validate($request,[
+            'name' => 'required',
+            'country' => 'required',
+            'status' => 'required',
+        ]);
+
+        $region->name = $data['name'];
+        $region->country_id = $data['country'];
+        $region->status = $data['status'];
+        $region->update();
+
+        Session::flash('statuscode', 'success');
+        return redirect('/regions')->with('status', 'Region has been updated.');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        $region = Region::findorfail($id);
+        $region->delete();
+
+        return redirect('/regions')->with('status', 'Region has been deleted');
     }
 }
