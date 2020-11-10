835949d4b7c431e5bf486c579eec26e2f23a7a09
yousaf arshad <yousoftme@gmail.com>
2020-11-10 18:00:12 +0500

    migrations commit only

---
+++ database/migrations/2020_11_10_175113_create_farm_cities_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateFarmCitiesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('farm_cities', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('city_id')->constrained();
+            $table->foreignId('farm_id')->constrained()->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('farm_cities');
+    }
+}
