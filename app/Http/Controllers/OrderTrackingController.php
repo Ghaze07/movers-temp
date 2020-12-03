843d88733a7b99d041ddf78c5f6a8ea3ffbe8ca2
yousaf arshad <yousoftme@gmail.com>
2020-12-03 12:59:13 +0500

    order placed mail view, my orders, order tracking view

--- app/Http/Controllers/OrderTrackingController.php
+++ app/Http/Controllers/OrderTrackingController.php
@@ -2,84 +2,24 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use App\Order;
+use App\OrderTracking;
+use Illuminate\Support\Facades\Auth;
 
 class OrderTrackingController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
     public function index()
     {
-        //
-        return view('order.tracking');
+            $user_id = Auth::user()->id;
+            $orders = Order::where(['user_id'=>$user_id])->with('OrderItems.farmProduct.product')->get();
+            
+        return view('myOrders.index',compact('orders'));
     }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
+    public function orderStatus($id)
     {
-        //
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request)
-    {
-        //
-    }
-
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function show($id)
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function edit($id)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, $id)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy($id)
-    {
-        //
+        $orderTracking = OrderTracking::where(['order_id'=> $id])->get();
+        return view('myOrders.orderStatus',compact('orderTracking'));
+        
     }
 }
--- resources/views/mail/orderplaced.blade.php
+++ resources/views/mail/orderplaced.blade.php
@@ -5,111 +5,228 @@
     <!-- Required meta tags -->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <title>ORDER PLACED</title>
+    <style>
 
-    <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
-
-    <title>Hello, world!</title>
+        .table {
+            width: 100%;
+            margin-bottom: 1rem;
+            color: #212529;
+        }
+        table{
+            border-collapse: collapse;
+        }
+        *, ::after, ::before {
+            box-sizing: border-box;
+        }
+        
+        .table thead th {
+            vertical-align: bottom;
+            border-bottom: 2px solid #dee2e6;
+        }
+                table, th, td {
+          border: 1px solid black;
+          border-collapse: collapse;
+          text-align: center;
+          padding: 5px;
+        }
+        .tdhover:hover {
+            background-color: #dee4e9!important;
+            color: white;
+        }
+        td:hover{
+            color:white;
+        }
+        li:hover{
+            background-color: #dee4e9!important;
+        }
+        .ulclass{
+            display: flex;
+            flex-direction: column;
+            justify-content: space-between;
+            list-style-position: inside;
+            border: 1px solid black;
+            padding: 10px;
+            margin: 5px;
+            align-content: justify;
+            text-align: justify;
+        }
+        li{
+            padding: 10px;
+            margin: 5px;
+            align-content: justify;
+            text-align: justify;
+        }
+        .column {
+          float: left;
+          width: 50%;
+          background-color:;
+          background-image: 
+        }
+        .column1{
+            width: 50%;
+        }
+        
+        /* Clear floats after the columns */
+        .row:after {
+          content: "";
+          display: table;
+          clear: both;
+          widows: 50%;
+          background-color:;
+        }
+        .maindiv{
+            background-image: url("pix/bg_panga.png");
+            background-color: #ccf1ff; 
+        }
+            </style>
+        
   </head>
   <body>
-    <h4>Dear {{ $farm_manager_name }}, </h4>
-    <h5>
-        A new order has been placed with following details.
-        {{--  Visit this link to view the order online {{oder_link_to_view}}. --}}
-    </h5>
-    <ul>
-        <li>
-            Order Number: {{ $order_number }}
-        </li>
-        <li>
-            Order Time: {{ $created_at }}
-        </li>
-        <li>
-            Order Status: {{ $status }}
-        </li>
-        <li>
-            Customer Name: {{ $user_name }}
-        </li>
-        <li>
-            Customer Mobile: {{ $user_mobile }}
-        </li>
-        <li>
-            Delivery Address: {{ $complete_address }}
-        </li>
-        <li>
-            Delivery Region: {{ $region_name }}
-        </li>
-        <li>
-            Delivery City: {{ $city_name }}
-        </li>
-        <li>
-            Recipient Name: {{ $receiver_name }}
-        </li>
-        <li>
-            Recipient Number: {{ $receiver_mobile }}
-        </li>
-        <li>
-            Order Value: {{ $order_total }}
-        </li>
-        <li>
-            Delivery Charges: {{ $delivery_charges }}
-        </li>
-        <li>
-            COD Amount: {{ $cash_on_delivery }}
-        </li>
-    </ul>
+    <div style="background-color:#F7DE6A; padding-top:8px;" class="row">
+        <div class="column1">
+            <h2 style="margin-left:15px" style="margin-top: 15px" style="padding: 10px;">Dear {{ $farm_manager_name }}, </h2>
+            <h4 style="margin-left:15px" style="padding: 10px" class="text-danger">
+                A new order has been placed with following details.
+                {{--  Visit this link to view the order online {{oder_link_to_view}}. --}}
+            </h4>
+        </div>
+          {{-- <div class="column1"><img src="pix/logo_full.png" alt="xyz"></div> --}}
+      </div>
+    
+    
+    <div class="maindiv" >
+        <ul type="none" class="ulclass" style="display: flex" style="justify-content: space-between;" style="flex-direction: column;">
+            <li style="color:black  ">
+                <div class="row">
+                    <div class="column">Order Number:</div>
+                    <div class="column">{{ $order_number }}</div>
+                </div>
+            </li>
 
-    <h4>Order Details: </h4>
-    <table>
-        <thead>
-            <tr>
-                <th>ID</th>
-                <th>Item Name</th>
-                <th>Item Quantity</th>
-                <th>Item Price</th>
-                <th>Item Total</th>
-            </tr>
-        </thead>
-        <tbody>
-            @foreach ($orderItems as $key => $orderItem)
-                <tr>
-                    <th>{{ $key+1 }}</th>
-                    <td>{{ $orderItem->farmProduct->product->name }}</td>
-                    <td>{{ $orderItem->quantity }}</td>
-                    <td>{{ $orderItem->farmProduct->unit_price }}</td>
-                    <td>{{ $orderItem->quantity * $orderItem->farmProduct->unit_price }}</td>
-                </tr> 
-            @endforeach
-            <tr>
-                <td colspan="3">Order Total</td>
-                <td colspan="2">{{ $order_total }}</td>
-            </tr>
-            <tr>
-                <td colspan="3">Delivery Charges</td>
-                <td colspan="2">{{ $delivery_charges }}</td>
-            </tr>
-            <tr>
-                <td colspan="3">Grand Total</td>
-                <td colspan="2">{{ $cash_on_delivery }}</td>
-            </tr>
-            
-        </tbody>
-    </table>
+            <li style="color:black;  ">
+                <div class="row">
+                    <div class="column"> Order Time: </div>
+                    <div class="column">{{ $created_at }}</div>
+                </div>
+            </li>
 
+            <li style="color:black;  ">
+                <div class="row">
+                    <div class="column"> Order Status: </div>
+                    <div class="column">{{ $status }}</div>
+                </div>
+            </li>
+
+            <li style="color:black;  ">
+                <div class="row">
+                    <div class="column">Customer Name: </div>
+                    <div class="column">{{ $user_name }}</div>
+                </div>
+            </li>
+
+            <li style="color:black;  ">
+                <div class="row">
+                    <div class="column"> Customer Mobile:</div>
+                    <div class="column">{{ '0'.$user_mobile }}</div>
+                </div>
+            </li>
+
+            <li style="color:black;  ">
+                <div class="row">
+                    <div class="column">Delivery Address:</div>
+                    <div class="column">{{ $complete_address }}</div>
+                </div>
+            </li>
+
+            <li style="color:black;  ">
+                <div class="row">
+                    <div class="column"> Delivery Region:</div>
+                    <div class="column"> {{ $region_name }}</div>
+                </div>
+            </li>
+
+            <li style="color:black;  ">
+                <div class="row">
+                    <div class="column">Delivery City:</div>
+                    <div class="column"> {{ $city_name }}</div>
+                </div>
+            </li>
+
+            <li style="color:black;  ">
+                <div class="row">
+                    <div class="column">Recipient Name:</div>
+                    <div class="column"> {{ $receiver_name }}</div>
+                </div>
+            </li>
+
+            <li style="color:black;  ">
+                <div class="row">
+                    <div class="column">Recipient Number:</div>
+                    <div class="column">{{ '0'.$receiver_mobile }}</div>
+                </div>
+            </li>
+
+            <li style="color:black;  ">
+                <div class="row">
+                    <div class="column">Order Value:</div>
+                    <div class="column">{{ $order_total }}</div>
+                </div>
+            </li>
+
+            <li style="color:black;  ">
+                <div class="row">
+                    <div class="column">Delivery Charges:</div>
+                    <div class="column">{{ $delivery_charges }}</div>
+                </div>
+            </li>
+
+            <li style="color:black;  ">
+                <div class="row">
+                    <div class="column">COD Amount:</div>
+                    <div class="column"> {{ $cash_on_delivery }}</div>
+                </div>
+            </li>
+        </ul>
+    </div>
+    <div style="overflow-x:auto;">
+        <h4>Order Details: </h4>
+        <table class="table table-hover"">
+            <thead class="thead-dark">
+                <tr style="background-color : #F7DE6A;" class=" border-dark">
+                    <th  style="color: black;" class="text-center">ID</th>
+                    <th  style="color: black;" class="text-center">Item Name</th>
+                    <th  style="color: black;" class="text-center">Item Quantity</th>
+                    <th  style="color: black;" class="text-center">Item Price</th>
+                    <th  style="color: black;" class="text-center">Item Total</th>
+                </tr>
+            </thead>
+            <tbody>
+                @foreach ($orderItems as $key => $orderItem)
+                    <tr class="trhover"  style="background-color:#ccf1ff;" class=" border-dark">
+                        <td style="color: black;" class="text-center">{{ $key+1 }}</td>
+                        <td style="color: black;" class="text-center">{{ $orderItem->farmProduct->product->name }}</td>
+                        <td style="color: black;" class="text-center">{{ $orderItem->quantity }}</td>
+                        <td style="color: black;" class="text-center">RS {{ $orderItem->farmProduct->unit_price }}</td>
+                        <td style="color: black;" class="text-center">RS {{ $orderItem->quantity * $orderItem->farmProduct->unit_price }}</td>
+                    </tr> 
+                @endforeach
+                <tr class="trhover" style="background-color:#ccf1ff;"  >
+                    <th style="color: red;" class="text-center" colspan="3">Order Total</th>
+                    <td style="color: red;"  class="text-center" colspan="2">RS {{ $order_total }}</td>
+                </tr>
+                <tr class="trhover" style="background-color:#ccf1ff;"  >
+                    <th style="color: red;" class="text-center" colspan="3">Delivery Charges</th>
+                    <td style="color: red;"  class="text-center" colspan="2">RS {{ $delivery_charges }}</td>
+                </tr>
+                <tr class="trhover" style="background-color:#ccf1ff;"  >
+                    <th style="color: red;" class="text-center" colspan="3">Grand Total</th>
+                    <td style="color: red;"  class="text-center" colspan="2">RS {{ $cash_on_delivery }}</td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
     <h5>Thanks </h5>
     <h4>{{$farm_name}}</h4>
-
-
-    <!-- Optional JavaScript; choose one of the two! -->
-
-    <!-- Option 1: jQuery and Bootstrap Bundle (includes Popper) -->
-    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous"></script>
-
-    <!-- Option 2: jQuery, Popper.js, and Bootstrap JS
-    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.min.js" integrity="sha384-w1Q4orYjBQndcko6MimVbzY0tgp4pWB4lZ7lr30WKz0vr/aWKhXdBNmNb5D92v7s" crossorigin="anonymous"></script>
-    -->
   </body>
 </html>
